e108997421c7dfff3088055a5e0d064ad82f3999
maya1996 <s.mayasurya@gmail.com>
2024-09-19 09:20:05 -0400

    added kmeans, working on fuzzy k, dbscan

--- clustering.py
+++ clustering.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 
+
 def boxplot_associate(df, indexes, graph_info_array):
     """
     :param df: The pandas dataframe containing the data to graph
@@ -52,11 +53,44 @@ def visual_eda(df):
     # Gender-Annual Income Association (Boxplot)
     boxplot_associate(df, (1,3), ["Gender", "Annual Income", "Gender-Annual Income Association"])
 
+#applying k-means
+def k_cluster(X, n_clusters=5):
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    clusters = kmeans.fit_predict(X)
+    # Finding the optimal number of clusters using the elbow method
+    wcss = []
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(df[["Age", "Annual Income (k$)", "Spending Score (1-100)"]])
+        wcss.append(kmeans.inertia_)
+
+    return clusters
+
+#Applying fuzzy-k
+def fuzzy_cmeans(X, n_clusters=5):
+    fcm = FCM(n_clusters=n_clusters)
+    fcm.fit(X)
+    clusters = fcm.predict(X)
+    return clusters
+
+def visualize_clusters(df, feature_x, feature_y, cluster_label):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df[feature_x], df[feature_y], c=df[cluster_label], cmap='viridis', s=50)
+    plt.title('K-Means Clustering with Gender')
+    plt.xlabel(feature_x)
+    plt.ylabel(feature_y)
+    plt.show()
+
+
+#
 
 def main():
     df = import_data()
     explore_data(df)
     visual_eda(df)
+    df['KMeans_Cluster'] = k_cluster(X_scaled, n_clusters=5)
+    visualize_clusters(df, 'Annul Income (k$)', 'Spending Score (1-100', 'k-Means Cluster')
+    df['FCM_Cluster'] = fuzzy_cmeans(X)
 
 
 if __name__ == '__main__':
