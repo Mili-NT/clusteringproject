19f3c9d35723f52472785f0fca151fc0eb0b17e8
Mili <55.980.11sec@gmail.com>
2024-09-22 13:34:40 -0400

    Changed elbow method to use silhouette score for ncluster predictions

--- clustering.py
+++ clustering.py
@@ -7,10 +7,8 @@ from fcmeans import FCM
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import silhouette_score
-from sklearn.mixture import GaussianMixture
-import skfuzzy as fuzz
 
-# TODO: DETERMINE APPROPRIATE NUMBER OF CLUSTERS INSTEAD OF HARDCODING
+# TODO: Improve clustering values
 
 def boxplot_associate(df, indexes, graph_info_array):
     """
@@ -60,57 +58,35 @@ def visual_eda(df):
     # Gender-Annual Income Association (Boxplot)
     boxplot_associate(df, (1,3), ["Gender", "Annual Income", "Gender-Annual Income Association"])
 
-# Placeholder Function for determining correct cluster count
-def nclusters_by_elbow_method(selected_features):
-    wcss = []
-    for i in range(1, 11):
-        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=50, random_state=0)
-        kmeans.fit(selected_features)
-        wcss.append(kmeans.inertia_)
-    print(wcss)
-    plt.figure(figsize=(10, 6))
-    plt.plot(range(1, 11), wcss)
-    plt.title('Elbow Method')
-    plt.xlabel('Number of Clusters')
-    plt.ylabel('WCSS')
-    plt.show()
-    # TODO: FINISH IMPLEMENTING ELBOW METHOD NCLUSTER SELECTION
-    # Currently, this returns the previous hardcoded ncluster value until we implement selecting from the inertia scores
-    # by calculating the gradient with diff
-    return 5
-
-# Function to apply KMeans and calculate silhouette score
-def kmeans_silhouette(selected_features, n_clusters):
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-    labels = kmeans.fit_predict(selected_features)
-    score = silhouette_score(selected_features, labels)
-    return score
+# Seperate ncluster determination for kmeans (elbow method did not produce desired results)
+def optimal_kmeans_clusters(selected_features):
+    optimal_score = -1
+    optimal_nclusters = 0
+    for n_clusters in range(2, 11):
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0)
+        labels = kmeans.fit_predict(selected_features)
+        if len(set(labels)) > 1:  # Has to be at least 1, ideally ~4 clusters based on sns plots
+            score = silhouette_score(selected_features, labels)
+            if score > optimal_score:
+                optimal_score = score
+                optimal_nclusters = n_clusters
+    return optimal_nclusters
 
 
-def k_cluster(selected_features, n_clusters=8):
-    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=300, n_init=50, random_state=0)
+def k_cluster(selected_features, n_clusters=5):
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
     clusters = kmeans.fit_predict(selected_features)
-    clusters_n = len(np.unique(clusters))
-    return clusters, clusters_n
+    return clusters
 
 #Applying fuzzy-k
 def fuzzy_cmeans(selected_features, n_clusters):
     fcm = FCM(n_clusters=n_clusters)
     fcm.fit(selected_features.values)
     clusters = fcm.predict(selected_features.values)
-    clusters_n = len(np.unique(clusters))
-    return clusters, clusters_n
-
-# Function to apply Fuzzy C-Means and calculate silhouette score
-def fuzzy_cmeans_silhouette(selected_features, n_clusters):
-    # Fuzzy C-Means algorithm using skfuzzy
-    cntr, u, _, _, _, _, _ = fuzz.cluster.cmeans(selected_features.T, n_clusters, 2, error=0.005, maxiter=1000, init=None)
-    labels = np.argmax(u, axis=0)  # Assign each point to the cluster with the highest membership
-    score = silhouette_score(selected_features, labels)
-    return score
+    return clusters
 
 #Applying DBSCAN Optimizing
-def dbscan_opt(selected_features, minPts, k=10 ):
+def dbscan_opt(selected_features, min_pts, k=10):
     """
     Plots the k-distance graph to help estimate the optimal eps for DBSCAN.
     Parameters:
@@ -126,7 +102,7 @@ def dbscan_opt(selected_features, minPts, k=10 ):
     plt.figure(figsize=(8, 5))
     plt.plot(k_distances)
     plt.xlabel('Data Points (sorted by distance)')
-    plt.ylabel(f'{minPts}-th Nearest Neighbor Distance')
+    plt.ylabel(f'{min_pts}-th Nearest Neighbor Distance')
     plt.title('k-distance Graph for DBSCAN')
     plt.grid(True)
     plt.show()
@@ -139,7 +115,7 @@ def dbscan_opt(selected_features, minPts, k=10 ):
     best_min_samples = None
     best_labels = None
 
-    for min_samples in minPts:
+    for min_samples in min_pts:
         dbscan = DBSCAN(eps=optimal_eps, min_samples=min_samples)
         labels = dbscan.fit_predict(selected_features)
 
@@ -154,132 +130,43 @@ def dbscan_opt(selected_features, minPts, k=10 ):
     return best_labels, optimal_eps, best_min_samples, best_score
 
     #Applying DBSCAN
-def dbscan(features, minPts, ep):
-    dbscan_model = DBSCAN(eps=ep, min_samples=minPts)
+def dbscan_clustering(features, min_pts, ep):
+    dbscan_model = DBSCAN(eps=ep, min_samples=min_pts)
     final_labels = dbscan_model.fit_predict(features)
-    count_clusters = len(set(final_labels)) - (1 if -1 in final_labels else 0)
 
-    return final_labels, count_clusters
-
-# Function to apply GMM and calculate silhouette score
-def gmm_silhouette(X, n_clusters):
-    gmm = GaussianMixture(n_components=n_clusters, random_state=0)
-    labels = gmm.fit_predict(X)
-    score = silhouette_score(X, labels)
-    return score
-
-#Finiding Optimial n components for Gaussian Matrix Mixture
-def find_best_n_components(selected_features):
-    bic = []
-    aic = []
-    n_components_range = range(1, 11)  # Try cluster counts from 1 to 10
-    for n in n_components_range:
-        gmm = GaussianMixture(n_components=n, random_state=0)
-        gmm.fit(selected_features)
-        bic.append(gmm.bic(selected_features))
-        aic.append(gmm.aic(selected_features))
-
-    # Plot BIC and AIC
-    plt.plot(n_components_range, bic, label='BIC')
-    plt.plot(n_components_range, aic, label='AIC')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('BIC/AIC')
-    plt.legend()
-    plt.show()
-
-    # Return the optimal number of components
-    optimal_n_components = n_components_range[bic.index(min(bic))]
-    return optimal_n_components
+    return final_labels
 
 
-def apply_gmm(X_scaled, n_components=3):
-    # Apply GMM with a specified number of components (clusters)
-    gmm = GaussianMixture(n_components=n_components, random_state=0)
-    clusters = gmm.fit_predict(X_scaled)
 
-    return clusters
-
-
-def calculate_silhouette_scores(selected_features, max_clusters=10):
-    kmeans_scores = []
-    gmm_scores = []
-    fuzzy_scores = []
-
-    for n in range(2, max_clusters + 1):
-        kmeans_scores.append(kmeans_silhouette(selected_features, n))
-        gmm_scores.append(gmm_silhouette(selected_features, n))
-        fuzzy_scores.append(fuzzy_cmeans_silhouette(selected_features, n))
-
-    return kmeans_scores, gmm_scores, fuzzy_scores
-
-def find_best_n_clusters(scores):
-    best_n_clusters = np.argmax(scores) + 2  # +2 because we start from 2 clusters
-    return best_n_clusters
-
-def visualize_clusters(df, feature_x, feature_y, cluster_label, count_cluster, sil_score):
+def visualize_clusters(df, feature_x, feature_y, cluster_label):
     plt.scatter(df[feature_x], df[feature_y], c=df[cluster_label], cmap='viridis', s=50)
     plt.title(cluster_label)
     plt.xlabel(feature_x)
     plt.ylabel(feature_y)
-    plt.text(0.95, 0.95, f'Clusters: {count_cluster}, \n Silhouette_Score: {sil_score}', fontsize=12, ha='center',
-             va='center',
-             transform=plt.gca().transAxes)
-    plt.show()
-
-
-# Function to plot silhouette score comparisons
-def plot_silhouette_comparison(kmeans_scores, gmm_scores, fuzzy_scores, best_scores):
-    clusters_range = range(2, len(kmeans_scores) + 2)
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(clusters_range, kmeans_scores, marker='o', label='KMeans')
-    plt.plot(clusters_range, gmm_scores, marker='x', label='GMM')
-    plt.plot(clusters_range, fuzzy_scores, marker='s', label='Fuzzy C-Means')
-    plt.plot(clusters_range, fuzzy_scores, marker='d', label='DBSCAN')
-    plt.title('Silhouette Score Comparison for Clustering Methods')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('Silhouette Score')
-    plt.legend()
     plt.show()
 
 
 def main():
     df = import_data()
     explore_data(df)
-    #visual_eda(df)
+    visual_eda(df)
 
     selected_features = df[["Age", "Annual Income (k$)", "Spending Score (1-100)"]]
+    kmeans_nclusters = optimal_kmeans_clusters(selected_features)
 
-    # Step 1: Calculate Silhouette Scores for different clustering methods
-    kmeans_scores, gmm_scores, fuzzy_scores = calculate_silhouette_scores(selected_features, max_clusters=10)
+    df['Kmeans_Cluster'] = k_cluster(selected_features, n_clusters=kmeans_nclusters)
+    visualize_clusters(df,'Annual Income (k$)','Spending Score (1-100)', 'Kmeans_Cluster')
 
-    # Step 2: Find the optimal number of clusters for each method
-    best_kmeans_n_clusters = find_best_n_clusters(kmeans_scores)
-    best_gmm_n_clusters = find_best_n_clusters(gmm_scores)
-    best_fuzzy_n_clusters = find_best_n_clusters(fuzzy_scores)
+    df['FCM_Cluster'] = fuzzy_cmeans(selected_features, n_clusters=5)
+    visualize_clusters(df, 'Annual Income (k$)' ,'Spending Score (1-100)','FCM_Cluster' )
 
-    df['Kmeans_Cluster'], count_clusters_kmeans = k_cluster(selected_features, n_clusters=best_kmeans_n_clusters)
-    df['FCM_Cluster'], count_clusters_FCM = fuzzy_cmeans(selected_features, n_clusters=best_fuzzy_n_clusters)
-    df['GMM_Cluster'] = apply_gmm(selected_features, n_components=best_gmm_n_clusters)
-
-
-
-    minPts = range(3, 15)
-    labels, optimal_eps, best_min_samples, best_score = dbscan_opt(selected_features, minPts)
-    df['DBSCAN_Cluster'], count_clusters_dbscan = dbscan(selected_features, best_min_samples, optimal_eps)
-
-    # Visualize the clusters for each method
-    visualize_clusters(df, 'Annual Income (k$)', 'Spending Score (1-100)', 'Kmeans_Cluster', count_clusters_kmeans,
-                       max(kmeans_scores))
-    visualize_clusters(df, 'Annual Income (k$)', 'Spending Score (1-100)', 'FCM_Cluster', count_clusters_FCM,
-                       max(fuzzy_scores))
-    visualize_clusters(df, 'Annual Income (k$)', 'Spending Score (1-100)', 'GMM_Cluster', best_gmm_n_clusters,
-                       max(gmm_scores))
-
-    visualize_clusters(df, 'Annual Income (k$)', 'Spending Score (1-100)', 'DBSCAN_Cluster', count_clusters_dbscan, best_score)
+    min_pts = range(3, 15)
+    labels, optimal_eps, best_min_samples, best_score = dbscan_opt(selected_features, min_pts)
+    df['DBSCAN_Cluster'] = dbscan_clustering(selected_features, best_min_samples, optimal_eps)
+    visualize_clusters(df, 'Annual Income (k$)',  'Spending Score (1-100)','DBSCAN_Cluster')
 
 
-    plot_silhouette_comparison(kmeans_scores, gmm_scores, fuzzy_scores, best_score)
+
 
 if __name__ == '__main__':
     main()
